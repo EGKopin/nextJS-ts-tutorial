5c481238af323e2a337e4d561b1fa0c3ed523881
Etana Kopin <claws.33@gmail.com>
2022-11-07 22:41:21 +0000

    image loader added and build and export works

--- tutorial_nextjs-ts/pages/_app.tsx
+++ tutorial_nextjs-ts/pages/_app.tsx
@@ -1,6 +1,12 @@
+//This file wraps every other page
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    Anything from _app is everywhere!
+  <Component {...pageProps} />
+  </>
+  );
 }
